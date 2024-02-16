2ccc06fc548877f0fbf0dcbb0089ae73bdff63a8
Manas Ranjan Adha <manas865873@gmail.com>
2024-02-16 21:11:45 +0530

    feat: dynamic content on email template

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = 5500;
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { Resend } = require("resend");
+const fs = require("fs");
 
 // Middleware to parse JSON in the request body
 app.use(express.json());
@@ -14,38 +15,42 @@ app.use(cors());
 const apiKey = process.env.RESEND_API_KEY;
 const resend = new Resend(apiKey);
 
-// const emailTo = "maheshbehera6970@gmail.com";
-// const emailTo = "manas865873@gmail.com";
-// const emailFrom = "manas865873@gmail.com";
-
 app.post("/sendemail", async (req, res) => {
 
     const receivedData = req.body;
     console.log('Received data:', receivedData);
 
-    const { data, error } = await resend.emails.send({
-        from: 'Manas <no-replay@goboolean.in>',
-        to: [`${receivedData.email}`],
-        subject: 'For Testing Purpose',
-        // html: `<h2>Hey, ${receivedData.name}, How are you?</h2>`,
-        html: `${receivedData.htmlTemplate}`,
+    // Read the email template from the file system
+    fs.readFile("./templates/email.html", "utf8", async (err, htmlTemplate) => {
+      if (err) {
+          console.error("Error reading email template file:", err);
+          return res.status(500).json({ success: false, error: "Error reading email template file" });
+      }
+
+      // Modify the email template with dynamic content
+      const modifiedTemplate = htmlTemplate
+      .replace("{{name}}", receivedData.name)
+      .replace("{{logoSrc}}", receivedData.logoSrc)
+      .replace("{{bannerSrc}}", receivedData.bannerSrc)
+      .replace("{{receiverUrl}}", receivedData.receiverUrl)
+
+
+      const { data, error } = await resend.emails.send({
+          from: 'Manas <no-replay@goboolean.in>',
+          to: [`${receivedData.email}`],
+          subject: 'For Testing Purpose',
+          html: modifiedTemplate, // Use the read HTML template
       });
-    
+
       if (error) {
-        console.log(error)
-        // res.send({error})
-        // return console.error({ error });
-        return res.status(400).json({ success: false, error });
+          console.error("Error sending email:", error);
+          return res.status(400).json({ success: false, error });
       }
 
-      if(data) {
-          console.log({ data });
-          // res.send({data})
-
-          res.status(200).json({success: true, data });
-      }
-        // res.send("Hello World");
-})
+      console.log("Email sent successfully:", data);
+      res.status(200).json({ success: true, data });
+  });
+});
 
 app.listen(PORT, () => {
     console.log("App is running on port:", PORT);
